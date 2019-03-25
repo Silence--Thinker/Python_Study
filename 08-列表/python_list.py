665129a243bff88104efb53c51014f826780dde2
silence <154261688@qq.com>
2019-03-26 00:44:36 +0800

    修改列表

--- 08-列表/python_list.py
+++ 08-列表/python_list.py
@@ -2,10 +2,10 @@
 #-*- coding: utf-8 -*-
 
 
-list = []
-list.append('Google')
-list.append("Runoob")
-print list
+list_temp = []
+list_temp.append('Google')
+list_temp.append("Runoob")
+print list_temp
 
 list1 = ['A', 'B', 'C']
 list2 = ['A', 'B', 'C', 'D']
@@ -18,3 +18,33 @@ list1.sort()
 print list1
 list1.sort(reverse=True)
 print list1
+
+array = list('ABCDEFG')
+array.append('H')
+print array
+array.pop()
+print array
+
+array2 = list('HIJK')
+array2.reverse()
+array.extend(array2)
+print array, ", length: ",len(array)
+
+#  排序
+array_tmep = sorted(array)
+print "sorted排序：", array_tmep, "\n", array
+
+array.sort()
+print "sort排序", array
+
+del array, array2, array_tmep
+
+array = list(['11231', '123123', '12', '1', 'BKHK', 'jjljlj;', 'kjaljdfa'])
+array_tmep = sorted(array, key = len, reverse = True)
+print "sorted排序：", array_tmep, "\n", array
+
+array.sort(key = len, reverse=False)
+print "sort排序", array
+
+
+
---
+++ 15-海龟绘图法/python_turtle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from turtle import *
+
+penup()
+forward(100)
+left(120)
+forward(100)
+left(120)
+forward(100)
+penup()
